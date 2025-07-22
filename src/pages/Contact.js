391ac5a13622a874ec33b257465b66efc6071128
Aditya <adityavardhan.s@wirtual.life>
2025-07-22 20:09:09 +0530

    contact-update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,34 @@
-import React from 'react';
-import './Contact.css'; // Add a separate CSS file for Contact page styling
+import React, { useRef,useState } from 'react';
+import emailjs from 'emailjs-com';
+import { Alert, Snackbar } from '@mui/material';
+import './Contact.css';
 
 const Contact = () => {
+  const form = useRef();
+   const [alert, setAlert] = useState({ open: false, severity: 'success', message: '' });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_vyeu6jp', // from EmailJS dashboard
+        'template_z7knbz4', // from EmailJS dashboard
+        form.current,
+        'dlq62CWkZS1TjLiAG' // from EmailJS dashboard
+      )
+     .then((result) => {
+      setAlert({ open: true, severity: 'success', message: 'Message sent successfully!' });
+      form.current.reset();
+    }, (error) => {
+      console.error(error);
+      setAlert({ open: true, severity: 'error', message: 'Failed to send message. Please try again.' });
+    });
+  };
+const handleClose = () => {
+    setAlert({ ...alert, open: false });
+  };
+
   return (
     <div className="contact">
       <h1>Contact Us</h1>
@@ -15,16 +42,29 @@ const Contact = () => {
             <li>Address: Chawla Complex, Zirakpur, Chandigarh Road, Near KMG Hotel Zirakpur-140603</li>
           </ul>
         </div>
-        <form className="contact-form">
-          <label>Name:</label>
+        <form ref={form} onSubmit={sendEmail} className="contact-form">
+          <label>Name :</label>
           <input type="text" name="name" placeholder="Your Name" required />
-          <label>Email:</label>
+          <label>Your Mobile No. :</label>
+           <input type="tel" name="phone" placeholder="Your Mobile No." required />
+          <label>Email :</label>
           <input type="email" name="email" placeholder="Your Email" required />
-          <label>Message:</label>
+          <label>Message :</label>
           <textarea name="message" placeholder="Your Message" required></textarea>
           <button type="submit">Submit</button>
         </form>
       </div>
+
+       <Snackbar
+        open={alert.open}
+        autoHideDuration={4000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity={alert.severity} sx={{ width: '100%' }}>
+          {alert.message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
